37292fa3f33f627021bc0d17a1005582a7b5a15a
siailya <sia.ilya.2017@gmail.com>
2023-08-27 08:29:15 +0300

    Add CLI local mode

--- cli/amm-cli.py
+++ cli/amm-cli.py
@@ -1,18 +1,29 @@
 import argparse
+import json
 import os
 
 import requests
 
+from ml_service import MLService
+
 
 def local_mode(model_path, query, use_file):
+    ml_service = MLService(model_path)
+
     if use_file:
         if not os.path.exists(query):
             print(f"File '{query}' does not exist.")
             return
-        with open(query, 'r') as file:
-            data = file.read()
+        with open(query, 'r', encoding="u8") as file:
+            data = file.read().replace("\r", "").split("\n")
+            result_json = []
+
+            for address in data:
+                result_json.append({"query": address, "result": ml_service.search_addresses(query)})
     else:
-        pass
+        result_json = ml_service.search_addresses(query)
+
+    print(json.dumps(result_json, indent=4, ensure_ascii=False))
 
 
 def api_mode(api_path, query):
@@ -21,7 +32,7 @@ def api_mode(api_path, query):
         response = requests.get(url)
         response.raise_for_status()
         result_json = response.json()
-        print(result_json)
+        print(json.dumps(result_json, indent=4, ensure_ascii=False))
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
@@ -30,7 +41,7 @@ def main():
     parser = argparse.ArgumentParser(description="AMM (Address Matching Machine) CLI")
     parser.add_argument("--mode", choices=["api", "local"], default="local", help="Select mode: api or local")
 
-    parser.add_argument("--model-path", type=str, help="Path to the model (local mode only)")
+    parser.add_argument("--model-path", type=str, default="./model/best_fast_text.model", help="Path to the model (local mode only)")
     parser.add_argument("--file", action="store_true", help="Use a file as input (local mode only)")
     parser.add_argument("query", type=str, nargs="?", help="Query string or file path (local mode only)")
 
---
+++ cli/ml_service.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from fast_text_wrapper import FastTextWrapper
+
+
+class MLService:
+    def __init__(self, model_path):
+        self.model_wrapper: FastTextWrapper = None
+        self.load_fasttext_model(model_path)
+
+    def load_fasttext_model(self, model_path: str):
+        print("Model loading")
+        self.model_wrapper = FastTextWrapper(
+            model_path,
+            town_index_path="data/additional_data/town_20230808.csv",
+            district_index_path="data/additional_data/district_20230808.csv",
+            street_abbv_index_path="data/additional_data/geonimtype_20230808.csv",
+            town_abbv_index_path="data/additional_data/subrf_20230808.csv",
+            building_index_path="data/additional_data/building_20230808.csv"
+        )
+
+    def _search_addresses(self, address: str, limit: int):
+        predictions = self.model_wrapper.predict(address, limit)
+        results = []
+
+        for p in predictions:
+            results.append({
+                "target_building_id": p[0],
+                "target_address": self.model_wrapper.get_address_from_id(p[0]).values[0],
+                "confident": p[1]
+            })
+
+        return results
+
+    def search_addresses(self, address: str, limit: int = 10):
+        return self._search_addresses(address, limit)
